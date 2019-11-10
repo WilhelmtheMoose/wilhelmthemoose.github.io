00f62afadf9781c7c77907661e013d7a3d7af5e9
WilhelmtheMoose <44333912+WilhelmtheMoose@users.noreply.github.com>
2019-11-10 01:18:02 -0500

    2

--- tfgame/protec.js
+++
@@ -1,8 +0,0 @@
-function validateKey(){
-    if(prompt('Please enter the key to access this page.') == 'pass123'){
-        return true;
-    }else{
-        alert('Incorrect key.');
-        return false;
-    }
-}
